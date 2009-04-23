aa3993efd99b3bca4b75ea5e797ec99835b309a8
Jacobo Aragunde Pérez <jaragunde@zebigbos.wired.citic.igalia.com>
2009-04-23 10:21:20 +0200

    Updated information about copyright and authors. This change gives proper credit to the latest contributors.

--- include/predefined_vars.php
+++ include/predefined_vars.php
@@ -1,4 +1,30 @@
 <?
+// PhpReport - A task reporting application
+//
+// Copyright (C) 2003-2009 Igalia, S.L. <info@igalia.com>
+//  Andrés Gómez García <agomez@igalia.com>
+//  Andrés Maneiro Boga <amaneiro@igalia.com>
+//  Enrique Ocaña González <eocanha@igalia.com>
+//  Jacobo Aragunde Pérez <jaragunde@igalia.com>
+//  José Riguera López <jriguera@igalia.com>
+//  Jesús Pérez Díaz <jperez@igalia.com>
+//  María Fernández Rodríguez <maria.fdez.rguez@gmail.com>
+//  Mario Sánchez Prada <msanchez@igalia.com>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
 require_once("autenticate.php");
 
 $actual_date=getdate(time());
